6b7d9ced71c3aaf11ee0977d8842ac236dd4bba1
Abhishek mishra <abhishekmishra992016@gmail.com>
2024-06-17 18:03:31 +0530

    task edit functionality added

--- src/pages/DataMatching/AdminAssined.jsx
+++ src/pages/DataMatching/AdminAssined.jsx
@@ -7,11 +7,17 @@ import {
 } from "../../services/common";
 import axios from "axios";
 import { toast } from "react-toastify";
+import AdminMatchingTasks from "./AdminMatchingTasks";
+import AdminCompareTasks from "./AdminCompareTasks";
+import TaskEdit from "./TaskEdit";
 
 const AdminAssined = () => {
   const [compareTask, setCompareTask] = useState([]);
   const [matchingTask, setMatchingTask] = useState([]);
   const token = JSON.parse(localStorage.getItem("userData"));
+  const [taskEdit, setTaskEdit] = useState(false);
+  const [allUsers, setAllUsers] = useState([]);
+  const [taskEditId, setTaskEditId] = useState("");
 
   useEffect(() => {
     const fetchCurrentUser = async () => {
@@ -78,7 +84,7 @@ const AdminAssined = () => {
         const tasks = await onGetAllTasksHandler();
         const templateData = await onGetTemplateHandler();
         const users = await onGetAllUsersHandler();
-
+        setAllUsers(users.users);
         const uploadTask = tasks.filter(
           (task) => task.moduleType === "Data Entry"
         );
@@ -257,6 +263,28 @@ const AdminAssined = () => {
     }
   };
 
+  const onEditTaskHandler = async (userId) => {
+    try {
+      const token = JSON.parse(localStorage.getItem("userData"));
+      await axios.post(
+        `http://${REACT_APP_IP}:4000/edit/assigned/task`,
+        { assignedTaskId: taskEditId, userId: userId },
+        {
+          headers: {
+            token: token,
+          },
+        }
+      );
+
+      toast.success("Task updated successfully.");
+      setTaskEditId("");
+      setTaskEdit(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  console.log(taskEditId);
 
   return (
     <div className="h-[100vh] flex justify-center items-center bg-gradient-to-r from-blue-700 to-purple-700 templatemapping pt-20">
@@ -299,229 +327,38 @@ const AdminAssined = () => {
                         <div className="py-3.5 text-center text-xl font-semibold text-gray-700 w-[150px]">
                           Download
                         </div>
+                        <div className="py-3.5 text-center text-xl font-semibold text-gray-700 w-[150px]">
+                          Edit
+                        </div>
+                        {/* <div className="py-3.5 text-center text-xl font-semibold text-gray-700 w-[150px]">
+                          Remove
+                        </div> */}
                       </div>
                     </div>
                     <div className="divide-y divide-gray-200 bg-white overflow-y-auto h-[250px]">
-                      {compareTask?.map((taskData) => (
-                        <div
-                          key={taskData.id}
-                          className="flex"
-                        >
-                          <div className="whitespace-nowrap  w-[150px] py-2">
-                            <div className="text-md text-center ">
-                              {taskData.name}
-                            </div>
-                          </div>
-                          <div className="whitespace-nowrap w-[150px] py-2">
-                            <div className="text-md text-center">
-                              {taskData.userName}
-                            </div>
-                          </div>
-                          <div className="whitespace-nowrap w-[80px] py-2">
-                            <div className="text-md text-center">
-                              {taskData.min}
-                            </div>
-                          </div>
-                          <div className="whitespace-nowrap w-[80px] py-2">
-                            <div className="text-md text-center">
-                              {taskData.max}
-                            </div>
-                          </div>
-                          <div className="whitespace-nowrap w-[200px] py-2">
-                            <div className="text-md text-center font-semibold border-2 py-1 ">
-                              {taskData.TemplateType}
-                            </div>
-                          </div>
-                          <div className="whitespace-nowrap w-[200px] py-2">
-                            <div className="text-md text-center ">
-                              <span
-                                className={`inline-flex items-center justify-center rounded-full ${
-                                  !taskData.blankTaskStatus ||
-                                  !taskData.multTaskStatus
-                                    ? "bg-amber-100 text-amber-700"
-                                    : "bg-emerald-100 text-emerald-700"
-                                } px-2.5 py-0.5`}
-                              >
-                                {!taskData.blankTaskStatus ||
-                                !taskData.multTaskStatus ? (
-                                  <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    strokeWidth="1.5"
-                                    stroke="currentColor"
-                                    className="-ms-1 me-1.5 h-4 w-4"
-                                  >
-                                    <path
-                                      strokeLinecap="round"
-                                      strokeLinejoin="round"
-                                      d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
-                                    />
-                                  </svg>
-                                ) : (
-                                  <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    strokeWidth="1.5"
-                                    stroke="currentColor"
-                                    className="-ms-1 me-1.5 h-4 w-4"
-                                  >
-                                    <path
-                                      strokeLinecap="round"
-                                      strokeLinejoin="round"
-                                      d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                                    />
-                                  </svg>
-                                )}
-                                <p className="whitespace-nowrap text-sm">
-                                  {taskData.blankTaskStatus &&
-                                  taskData.multTaskStatus
-                                    ? "Completed"
-                                    : "Pending"}
-                                </p>
-                              </span>
-                            </div>
-                          </div>
-                          <div className="whitespace-nowrap text-center w-[200px] py-2">
-                            <button
-                              className={`rounded px-4 py-1 font-semibold ${
-                                taskData.blankTaskStatus &&
-                                taskData.multTaskStatus
-                                  ? "bg-teal-300"
-                                  : "bg-gray-400 text-gray-600 cursor-not-allowed"
-                              }`}
-                              disabled={
-                                !taskData.blankTaskStatus ||
-                                !taskData.multTaskStatus
-                              }
-                            >
-                              Start Again
-                            </button>
-                          </div>
-                          <div className="whitespace-nowrap text-center w-[200px] py-2">
-                            <button
-                              onClick={() =>
-                                onCompareTaskStartHandler(taskData)
-                              }
-                              className="rounded border border-indigo-500 bg-indigo-500 px-4 py-1 font-semibold text-white"
-                            >
-                              Download
-                            </button>
-                          </div>
-                        </div>
-                      ))}
-                      {matchingTask?.map((taskData) => (
-                        <div key={taskData.id} className="flex ">
-                          <div className="whitespace-nowrap w-[150px] py-2">
-                            <div className="text-center text-md ">
-                              {taskData.templateName}
-                            </div>
-                          </div>
-                          <div className="whitespace-nowrap w-[150px] py-2">
-                            <div className="text-center text-md">
-                              {taskData.userName}
-                            </div>
-                          </div>
-                          <div className="whitespace-nowrap w-[80px] py-2">
-                            <div className="text-md text-center">
-                              {taskData.min}
-                            </div>
-                          </div>
-                          <div className="whitespace-nowrap w-[80px] py-2">
-                            <div className="text-md text-center">
-                              {taskData.max}
-                            </div>
-                          </div>
-                          <div className="whitespace-nowrap w-[150px] py-2">
-                            <div className="text-md text-center font-semibold border-2 py-1">
-                              {taskData.moduleType}
-                            </div>
-                          </div>
-                          <div className="whitespace-nowrap w-[150px] py-2">
-                            <div className="text-md text-center">
-                              <span
-                                className={`inline-flex items-center justify-center rounded-full ${
-                                  !taskData.blankTaskStatus ||
-                                  !taskData.multTaskStatus
-                                    ? "bg-amber-100 text-amber-700"
-                                    : "bg-emerald-100 text-emerald-700"
-                                } px-2.5 py-0.5`}
-                              >
-                                {!taskData.blankTaskStatus ||
-                                !taskData.multTaskStatus ? (
-                                  <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    strokeWidth="1.5"
-                                    stroke="currentColor"
-                                    className="-ms-1 me-1.5 h-4 w-4"
-                                  >
-                                    <path
-                                      strokeLinecap="round"
-                                      strokeLinejoin="round"
-                                      d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
-                                    />
-                                  </svg>
-                                ) : (
-                                  <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    strokeWidth="1.5"
-                                    stroke="currentColor"
-                                    className="-ms-1 me-1.5 h-4 w-4"
-                                  >
-                                    <path
-                                      strokeLinecap="round"
-                                      strokeLinejoin="round"
-                                      d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                                    />
-                                  </svg>
-                                )}
-                                <p className="whitespace-nowrap text-sm mt-1">
-                                  {taskData.blankTaskStatus &&
-                                  taskData.multTaskStatus
-                                    ? "Completed"
-                                    : "Pending"}
-                                </p>
-                              </span>
-                            </div>
-                          </div>
-                          <div className="whitespace-nowrap text-center w-[150px] py-2">
-                            <button
-                              onClick={() => onCompleteHandler(taskData)}
-                              className={`rounded-3xl px-4 py-1 font-semibold ${
-                                taskData.blankTaskStatus &&
-                                taskData.multTaskStatus
-                                  ? "bg-indigo-500 text-white border border-indigo-500"
-                                  : "bg-gray-400 text-gray-600 cursor-not-allowed"
-                              }`}
-                              disabled={
-                                !taskData.blankTaskStatus ||
-                                !taskData.multTaskStatus
-                              }
-                            >
-                              Start Again
-                            </button>
-                          </div>
-                          <div className="whitespace-nowrap text-center w-[150px] py-2">
-                            <button
-                              onClick={() => onDownloadHandler(taskData)}
-                              className="rounded-3xl border border-indigo-500 bg-indigo-500 px-4 py-1 font-semibold text-white"
-                            >
-                              Download
-                            </button>
-                          </div>
-                        </div>
-                      ))}
+                      <AdminCompareTasks
+                        compareTask={compareTask}
+                        onCompareTaskStartHandler={onCompareTaskStartHandler}
+                      />
+                      <AdminMatchingTasks
+                        onCompleteHandler={onCompleteHandler}
+                        setTaskEdit={setTaskEdit}
+                        matchingTask={matchingTask}
+                        onDownloadHandler={onDownloadHandler}
+                        setTaskEditId={setTaskEditId}
+                      />
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
+          <TaskEdit
+            taskEdit={taskEdit}
+            setTaskEdit={setTaskEdit}
+            allUsers={allUsers}
+            onEditTaskHandler={onEditTaskHandler}
+          />
         </section>
       </div>
     </div>
---
+++ src/pages/DataMatching/TaskEdit.jsx
@@ -0,0 +1,110 @@
+import React from "react";
+import { FaRegUser } from "react-icons/fa";
+
+const TaskEdit = ({ taskEdit, setTaskEdit, allUsers, onEditTaskHandler }) => {
+  return (
+    <div>
+      {taskEdit && (
+        <div className="fixed z-50 inset-0 overflow-y-auto">
+          <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            {/* Background overlay */}
+            <div
+              className="fixed inset-0 transition-opacity"
+              aria-hidden="true"
+            >
+              <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+            </div>
+
+            <span
+              className="hidden sm:inline-block sm:align-middle sm:h-screen"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+              <div className="bg-gradient-to-r from-blue-500 to-purple-500 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <div className="sm:flex sm:items-start">
+                  <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                    <svg
+                      onClick={() => setTaskEdit(false)}
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="h-6 w-6 text-red-600 cursor-pointer"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M6 18L18 6M6 6l12 12"
+                      ></path>
+                    </svg>
+                  </div>
+                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <h3
+                      className="text-lg font-medium text-white"
+                      id="modal-title"
+                    >
+                      Edit Task
+                    </h3>
+                    <div className="mt-2">
+                      <p className="text-sm text-white">
+                        Assign this task to a user.
+                      </p>
+                      <div className="mt-4 max-h-64 overflow-y-auto bg-white p-4 rounded-md shadow-inner">
+                        {allUsers.map((user) => (
+                          <>
+                            {user.role !== "Admin" && (
+                              <div
+                                key={user.id}
+                                className="flex items-center justify-between p-2 border-b hover:bg-gray-100 rounded-md transition duration-150"
+                              >
+                                <div className="flex items-center">
+                                  <span
+                                    className="h-12 w-12 rounded-full object-cover mt-4"
+                                    src={<FaRegUser />}
+                                    alt={user.userName}
+                                  >
+                                    {<FaRegUser />}
+                                  </span>
+                                  <span className="ml-3 font-medium text-gray-700">
+                                    {user.userName}
+                                  </span>
+                                  <span className="ml-3 font-medium text-gray-700">
+                                    {user.email}
+                                  </span>
+                                </div>
+                                <button
+                                  onClick={() => onEditTaskHandler(user.id)}
+                                  className="ml-2 inline-flex justify-center w-auto rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-500 text-base font-medium text-white hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:text-sm"
+                                >
+                                  Assign
+                                </button>
+                              </div>
+                            )}
+                          </>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="flex justify-end py-3 px-3 bg-gray-100">
+                <button
+                  onClick={() => setTaskEdit(false)}
+                  type="button"
+                  className="inline-flex justify-center cursor-pointer rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
+                >
+                  Cancel
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default TaskEdit;
